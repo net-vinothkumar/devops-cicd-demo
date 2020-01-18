6b8da8ab2a14598d77eec561f00ae143d1b13727
vinothkumar <net.vinothkumar@gmail.com>
2020-01-18 17:55:37 +0100

    new cicd demo

---
+++ src/main/java/com/example/cicddemo/CicdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicddemo/CicdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
