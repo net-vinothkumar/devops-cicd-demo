b6eef349d370314203a9a44207d3d9a0eb13597f
vinothkumar <net.vinothkumar@gmail.com>
2020-01-19 15:09:20 +0100

    add a print statement

--- src/main/java/com/example/cicddemo/CicdDemoApplication.java
+++ src/main/java/com/example/cicddemo/CicdDemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CicdDemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("sample cicd project..");
 		SpringApplication.run(CicdDemoApplication.class, args);
 	}
 
